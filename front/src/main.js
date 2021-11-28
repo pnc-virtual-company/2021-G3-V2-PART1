67bc7f9d304b58331df4f97fb3ac13652d6474b4
SomounOm <somoun.om077@gmail.com>
2021-11-28 19:12:52 +0700

    #19 manu bar html

--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Nav from "./components/nav/NavBar.vue";
+import MyEvent from "./components/myevent/MyEvent.vue";
+import Category from "./components/categories/Category.vue";
+import FindEvent from "./components/findevent/FindEvent.vue";
+import AllEvent from "./components/allevent/Allevent.vue";
+
+
+
+const app = createApp(App);
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: Nav},
+        { path: '/myevent', component: MyEvent},
+        { path: '/findEvent', component: FindEvent},
+        { path: '/category', component: Category },
+        { path: '/allevent', component: AllEvent },
+    ],
+});
+app.use(router)
+app.mount("#app");
