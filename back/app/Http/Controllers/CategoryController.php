0382a00411599e5b0ba9d384cb9c74f8ee97a7ee
nimout mang <nimout.mang@student.passerellesnumeriques.org>
2021-12-01 18:32:04 +0700

    #26 Manage category on backend

---
+++ back/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Category::all();
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $category = new Category();
+        $category->categoryName = $request->categoryName;
+        $category->save();
+
+        return response()->json(['Message' => 'Create Category Succesfully'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Category::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //Create Validation
+        $request->validate([
+            'categoryName' => 'min:4|max:20'
+        ]);
+
+        $category =  Category::findOrFail($id);
+        $category->categoryName = $request->categoryName;
+        $category->save();
+
+        return response()->json(['Message' => 'Update Category Succesfully'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Category::destroy($id);
+    }
+}
